23ca9186d36f86e53f867384ad00f930dc6aaa50
Adriana643 <151708687+Adriana643@users.noreply.github.com>
2025-02-21 11:35:24 -0500

    Remove comments script.js
    Comments were not necessary :)

--- frontend/script.js
+++ frontend/script.js
@@ -5,26 +5,22 @@ document.addEventListener("DOMContentLoaded", function() {
     const closeRegister = document.getElementById("closeRegister");
     const backToLogin = document.getElementById("backToLogin");
 
-    // Open Register Modal
     openRegister.addEventListener("click", function(event) {
         event.preventDefault();
         loginWrapper.style.display = "none";
         registerWrapper.style.display = "block";
     });
 
-    // Close Register Modal
     closeRegister.addEventListener("click", function() {
         registerWrapper.style.display = "none";
         loginWrapper.style.display = "block";
     });
 
-    // Go back to Login
     backToLogin.addEventListener("click", function(event) {
         registerWrapper.style.display = "none";
         loginWrapper.style.display = "block";
     });
 
-    // Close modal when clicking outside of it
     window.addEventListener("click", function(event) {
         if (event.target === registerForm) {
         registerWrapper.style.display = "none";
