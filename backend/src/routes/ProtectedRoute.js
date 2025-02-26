7855bea6dabdcf3b2fd20b5b69015117e50a18dd
Adriana643 <a.d.r.i@hotmail.ca>
2025-02-25 21:13:37 -0500

    Adapt user authentication to database

--- backend/src/routes/ProtectedRoute.js
+++ backend/src/routes/ProtectedRoute.js
@@ -18,16 +18,20 @@ router.post("/api/auth/register", async (req, res) => {
     });
 
     //Adding data to our own table
-    const { id } = data.user;
-    const { error: insertError } = await supabase
+    const { error: databaseError } = await supabase
         .from("Users")
-        .insert([{ id, email, password }]);
+        .insert([{
+                user_id: data.user?.id,
+                Created_at: new Date().toISOString(),
+                email: email, 
+                password: password 
+            }]);
 
     if (error) {
         return res.status(400).json({ msg: error.message });
     }
-    if (insertError) {
-        return res.status(500).json({ error: insertError.message });
+    if (databaseError) {
+        return res.status(500).json({ error: databaseError.message });
     }
 
     res.status(201).json({ msg: "User registered successfully", user: data.user });
