25a7c6338f8ab52db01f58c4b75fa8c780d4e91b
Adriana643 <a.d.r.i@hotmail.ca>
2025-03-05 01:19:30 -0500

    Create call to send direct messages
    When the function is called along with a request containing the email of the receiver of the message, and the message itself, then the message will be saved in the database.
    
    Example of request format:
    {
        "email":"testuser2@gmail.com",
        "message": "Hello John! Where's Jane?"
    }

--- backend/src/routes/MessagingRoute.js
+++ backend/src/routes/MessagingRoute.js
@@ -2,40 +2,70 @@ import express from "express";
 const router = express.Router();
 import { supabase } from "../server.js";
 
-//Retrieve private DM
+//Retrieve private DM (When user is receiving message)
 
 
 
-//Save private DM
+//Save private DM (When user is sending message)
 router.post("/dm/save", async (req, res) => {
 
-    const { data: { user }, error } = await supabase.auth.getUser();
-
-    const { email } = req.body;
+    const { data: { user }, error } = await supabase.auth.getUser(req.headers.authorization?.split(" ")[1]);
 
     if (error || !user) {
-        return res.status(401).send("Unauthorized");
+        return res.status(401).json(error);
       }
 
-    const { message } = req.body;
+    const { email, message } = req.body;
+
+    const receiverInfo = await fetch(`http://localhost:4000/api/userid/${email}`);
+    const receiverData = await receiverInfo.json();
+    const receiverId = receiverData.data.user_id;
+
+    if (!receiverInfo) {
+        return res.status(404).json({ error: "User not found"});
+    }
 
     const { data, error: databaseError } = await supabase
         .from('DMs')
         .insert([
             {
-                user_id: user.id,
-                message: message,
-                created_at: new Date().toISOString(),
+                BubblerID: user.id,
+                PopperID: receiverId,
+                Message: message,
+                Timestamp: new Date().toISOString(),
             }
             ]);
 
     if (databaseError) {
-        return res.status(500).send("Message could not be saved.");
+        return res.status(500).json({msg:"Message could not be saved.", databaseError});
     }
 
-    res.status(200).send("Message was saved successfully");
+    res.status(200).json({msg:"Message was saved successfully", data});
 
 });
+
+
+//Retrieve User uuid using a user's email
+router.get("/api/userid/:email", async (req, res) => {
+
+    const { email } = req.params;
+
+    if (!email) {
+        return res.status(400).json({ error: "Email was not received." });
+    }
+    const { data, error } = await supabase
+    .from('Users')
+    .select('user_id') 
+    .eq('email', email)
+    .single();
+
+    if (error) {
+        return res.status(400).json({ msg: error.message });
+    }
+
+    res.status(200).json({ msg: "Uuid was retrieved.", data });
+
+})
 
 
 
