02035c0b0d667a7812c43c51803645b3291917c4
Adriana643 <a.d.r.i@hotmail.ca>
2025-03-13 00:28:13 -0400

    #64 Complete deletion/addition of galleries backend

--- backend/src/routes/GalleryRoute.js
+++ backend/src/routes/GalleryRoute.js
@@ -2,6 +2,53 @@ import express from "express";
 const router = express.Router();
 import { supabase } from "../server.js";
 
+//Deletion of a gallery
+router.delete("/gal/delete", async (req, res) => {
+
+    const { data: { user }, error } = await supabase.auth.getUser(req.headers.authorization?.split(" ")[1]);
+
+    if (error || !user) {
+        return res.status(401).json(error);
+    }
+    const { galleryName } = req.body;
+
+    //Convert gallery name to gallery ID
+    const galleryID = await fetch(`http://localhost:4000/gal/getID/${galleryName}`);
+    const galleryIDres = await galleryID.json();
+    const gallId = galleryIDres.data.GalleryID;
+
+    //Helper call
+    const verifyUser = await fetch('http://localhost:4000/gal/verifyCreator', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          UserID: user.id,
+          GalleryID: gallId,
+        }),
+      });
+    const verifyUserResponse = await verifyUser.json();
+
+    if(verifyUserResponse.GalleryRole){
+
+        const { data, error: databaseError } = await supabase
+        .from('Galleries')
+        .delete()
+        .eq('GalleryID', gallId);
+
+        if (databaseError) {
+            return res.status(500).json({msg:"Gallery could not be deleted.", databaseError});
+        }
+
+        res.status(200).json({msg:"Gallery was successfully deleted."});
+
+    }else{
+        res.status(200).json({msg:"User is not owner of the gallery."});
+    }
+
+});
+
 //Creation of a gallery
 router.post("/gal/create", async (req, res) => {
 
@@ -10,10 +57,6 @@ router.post("/gal/create", async (req, res) => {
     if (error || !user) {
         return res.status(401).json(error);
     }
-
-    console.log(user);
-
-    console.log( req.body);
 
     const { galleryName } = req.body;
 
@@ -26,37 +69,45 @@ router.post("/gal/create", async (req, res) => {
                 Creator_id: user.id
             }
             ])
-        .select("GalleryID");
-
-    console.log(data);    
+        .select("GalleryID")
+        .single();
 
     if (databaseError) {
         return res.status(500).json({msg:"Gallery could not be created.", databaseError});
     }
 
-    const addUser = await fetch(`http://localhost:4000/gal/addCreator/${data.GalleryID}`);
+    const gallId = data.GalleryID;
 
+    //Helper call
+    const addUser = await fetch('http://localhost:4000/gal/addCreator', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          UserID: user.id,
+          GalleryID: gallId,
+        }),
+      });
+
+    const addUserResult = await addUser.json();
+    
     res.status(200).json({msg:"Gallery was successfully created.", data});
 
 });
 
-router.post("/gal/addCreator/:galleryID", async (req, res) => {
 
-    const { data: { user }, error } = await supabase.auth.getUser(req.headers.authorization?.split(" ")[1]);
+//Helper call to add the creator of the gallery inside the GalleryMembers table
+router.post("/gal/addCreator", async (req, res) => {
 
-    if (error || !user) {
-        return res.status(401).json(error);
-    }
-
-    const { galleryID } = req.body;
+    const { UserID, GalleryID } = req.body;
 
     const { data, error: databaseError } = await supabase
         .from('GalleryMembers')
         .insert([
             {
-                GalleryID: galleryID,
-                UserID: user.id,
-                Created_at: new Date().toISOString(),
+                GalleryID: GalleryID,
+                UserID: UserID,
                 GalleryRole: true
             }
             ]);
@@ -66,6 +117,51 @@ router.post("/gal/addCreator/:galleryID", async (req, res) => {
     }
 
     res.status(200).json({msg:"Gallery member has been recorded as creator.", data});
+
+});
+
+//Helper method to verify is user can delete gallery
+router.post("/gal/verifyCreator", async (req, res) => {
+
+    const { UserID, GalleryID } = req.body;
+
+    const { data, error: databaseError } = await supabase
+    .from('Galleries')
+    .select('*')
+    .eq('GalleryID', GalleryID)
+    .eq('Creator_id', UserID);
+
+    if (databaseError) {
+        return res.status(500).json({msg:"Could not verify gallery member role.", databaseError});
+    }
+
+    if(data && data.length !== 0 ){
+        res.status(200).json({msg:"Gallery member is creator of the gallery. Procceeding with deletion", "GalleryRole": true });
+    }else{
+        return res.status(200).json({msg:"Gallery member is not creator of the gallery. Cannot delete gallery", "GalleryRole": false});
+    }
+
+});
+
+//Helper method to get gallery ID from gallery name
+router.get("/gal/getID/:galleryName", async (req, res) => {
+
+    const { galleryName } = req.params;
+
+    if (!galleryName) {
+        return res.status(400).json({ error: "Gallery name was not received." });
+    }
+    const { data, error } = await supabase
+    .from('Galleries')
+    .select('GalleryID') 
+    .eq('GalleryName', galleryName)
+    .single();
+
+    if (error) {
+        return res.status(400).json({ msg: error.message });
+    }
+
+    res.status(200).json({ msg: "Gallery ID was retrieved.", data });
 
 });
 
