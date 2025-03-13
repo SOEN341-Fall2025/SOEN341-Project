fa055117a0852ff66e7e284223d00e67f82135f7
Steven01231 <dysteven1@gmail.com>
2025-03-13 15:21:26 -0400

    sasa

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -257,6 +257,7 @@ function App() {
     // Here, you can add authentication logic (API call or checking credentials)
     // For now, just set it to true to simulate successful login
 
+<<<<<<< Updated upstream
     console.log("DEBUG: handleLogin called with:", email, password);
 
     try{
@@ -282,6 +283,28 @@ function App() {
     }catch(error){
       console.error("There was an error during login.");
     }
+=======
+          
+          // Step 2: Get user info using token
+          const userResponse = await fetch('/api/auth/me', {
+            method: 'GET',
+            headers: { Authorization: `Bearer ${token}` }, // Pass token in Authorization header
+          });
+          if (!userResponse.ok) throw new Error('Failed to fetch user info');
+          const userInfo = await userResponse.json();
+          setUserData(userInfo); // Save user info
+          //console.log("userResponse Success:", userInfo);   
+          
+          
+          setIsLoggedIn(true); // Mark as logged in 
+          // Example usage after login
+          setCookie('authToken', token, 1); // Cookie valid for 7 days          
+          
+        } catch (error) {
+          console.error("Error during login:", error);
+          setIsLoggedIn(false);
+        }      
+>>>>>>> Stashed changes
   };
 
   return (
