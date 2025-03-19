d4c2334e0b81b72dcb3a1e416dca9b12097f10f4
xidxnx <abdybaevaaidana11@gmail.com>
2025-03-19 17:19:27 -0400

    update login.js

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -7,11 +7,11 @@ function Login({ onLogin }) {
     const [password, setPassword] = useState('');
     const [showPassword, setShowPassword] = useState(false);
     const [showLogin, setShowLogin] = useState(true);
-    const [registerPassword, setRegisterPassword] = useState('');
-    const [confirmPassword, setConfirmPassword] = useState('');
+  const [registerPassword, setRegisterPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
     const [passwordStrength, setPasswordStrength] = useState('Weak');
     const [passwordMatch, setPasswordMatch] = useState(true);
-
+  
     const handleSubmit = (e) => {
         e.preventDefault();
         if (showLogin) {
@@ -26,7 +26,7 @@ function Login({ onLogin }) {
 
     // Toggle between login and register views
     const handleToggleView = () => setShowLogin(!showLogin);
-
+    
     // Toggle password visibility
     const togglePasswordVisibility = () => setShowPassword(!showPassword);
 
@@ -55,58 +55,58 @@ function Login({ onLogin }) {
     };
 
     return (
-        <div>
-            {showLogin ? (
+    <div>
+        {showLogin ? (
                 <div className="wrapper">
                     <form onSubmit={handleSubmit}>
-                        <h1>Login</h1>
+                <h1>Login</h1>
                         <div className="input-box">
                             <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} required />
                             <i className='bx bxs-user'></i>
-                        </div>
+                </div>
                         <div className="input-box">
                             <input type={showPassword ? "text" : "password"} placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} required />
                             <i className={`bx ${showPassword ? 'bx bxs-lock-open-alt' : 'bx bxs-lock-alt'}`} onClick={togglePasswordVisibility}></i>
-                        </div>
+                </div>
                         <div className="text">
                             <label><input type="checkbox" />Remember Me</label>
-                            <a href="#">Forgot Password</a>
-                        </div>
+                    <a href="#">Forgot Password</a>
+                </div>
                         <button type="submit" className="button">Login</button>
                         <div className="register-link">
                             <p>Don't have an account? <a onClick={handleToggleView}>Register</a></p>
-                        </div>
-                    </form>
                 </div>
+            </form>
+            </div>
             ) : (
                 <div className="wrapper">
                     <form>
-                        <h1>Sign Up</h1>
+                    <h1>Sign Up</h1>
                         <div className="input-box">
                             <input type="text" placeholder="Username" required />
                             <i className='bx bxs-user'></i>
-                        </div>
+                    </div>
                         <div className="input-box">
                             <input type="email" placeholder="Email" required />
                             <i className='bx bxs-envelope'></i>
-                        </div>
+                    </div>
                         <div className="input-box">
                             <input type={showPassword ? "text" : "password"} placeholder="Create password" value={registerPassword} onChange={handlePasswordChange} required />
                             <i className={`bx ${showPassword ? 'bx bxs-lock-open-alt' : 'bx bxs-lock-alt'}`} onClick={togglePasswordVisibility}></i>
-                        </div>
+                    </div>
                         <div className="input-box">
                             <input type={showPassword ? "text" : "password"} placeholder="Confirm password" value={confirmPassword} onChange={handleConfirmPassword} required />
                             <i className={`bx ${showPassword ? 'bx bxs-lock-open-alt' : 'bx bxs-lock-alt'}`} onClick={togglePasswordVisibility}></i>
-                        </div>
+              </div>
                         {!passwordMatch && <p style={{ color: '#0052A5' }}> Passwords do not match</p>}
                         <button type="submit" className="button" disabled={!passwordMatch}>Sign Up</button>
                         <div className="login-link">
                             <p>Already have an account? <a onClick={handleToggleView}>Login</a></p>
-                        </div>
-                    </form>
-                </div>
-            )}
-        </div>
+                    </div>
+                </form>
+            </div>
+        )}
+    </div>
     );
 }
 export default Login;
