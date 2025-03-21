4ad690a44acf40d59d7d63492b926ac3b9af89cc
Adriana643 <a.d.r.i@hotmail.ca>
2025-03-21 11:17:09 -0400

    #5 - Backend call to retrieve all channel messages

--- backend/src/routes/GalleryRoute.js
+++ backend/src/routes/GalleryRoute.js
@@ -262,6 +262,77 @@ router.post("/gal/createChannel", async (req, res) => {
 
 });
 
+//Retrieving channel messages
+router.get("/gal/msgChannel", async (req, res) => {
+
+    const { data: { user }, error } = await supabase.auth.getUser(req.headers.authorization?.split(" ")[1]);
+
+    if (error || !user) {
+        return res.status(401).json(error);
+    }
+
+    const { channelName } = req.body;
+
+    const galIDresponse = await fetch(`http://localhost:4000/api/get/galleryID-channelName/${channelName}`);
+    const galleryIDdata = await galIDresponse.json();
+    const galleryID = galleryIDdata.data.GalleryID;
+
+    const { data, error: databaseError } = await supabase
+        .from('ChannelMessages')
+        .select('*')
+        .eq("Gallery_id", galleryID)
+        .eq("Channel_name", channelName);
+
+    if (databaseError) {
+        return res.status(500).json({msg:"Messages could not be fetched.", databaseError});
+    }
+
+    const updatedData = [];
+
+    for (let values of data) {
+        // Fetch the sender's username
+        const usernameResponse = await fetch(`http://localhost:4000/api/get/username-id/${values.User_id}`);
+        const usernameData = await usernameResponse.json();
+        const username = usernameData.data.username;
+
+        // Fetch the Bubbler's username
+        const msg = values.msg;
+
+        // Add the converted usernames to the DM object
+        updatedData.push({
+            ...values,
+            Username: username,
+            Message: msg
+        });
+    }
+
+    res.json({ msg: "Channel messages were fetched.", updatedData });
+
+});
+
+//Helper call: Retrieve gallery ID using channel's name
+router.get("/api/get/galleryID-channelName/:channelName", async (req, res) => {
+
+    const { channelName } = req.params;
+
+    if (!channelName) {
+        return res.status(400).json({ error: "Channel name was not received." });
+    }
+    
+    const { data, error } = await supabase
+    .from('Channels')
+    .select('GalleryID') 
+    .eq('ChannelName', channelName)
+    .single();
+
+    if (error) {
+        return res.status(400).json({ msg: error.message });
+    }
+
+    res.status(200).json({ msg: "Gallery ID was retrieved.", data });
+
+});
+
 router.get("/api/gallery/all", async (req, res) => {
     const token = req.header("Authorization")?.split(" ")[1];
     if (!token) {
@@ -342,7 +413,7 @@ router.get("/api/gallery/channels", async (req, res) => {
       res.status(500).json({ error: "Internal Server Error" });
     }
 });
-
+ 
 router.get("/api/gallery/getChannels", async (req, res) => {
     const token = req.header("Authorization")?.split(" ")[1];
     if (!token) {
